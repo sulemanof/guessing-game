937dfbbb8484cfbf85d4c0e18de9f60a561a9cb3
Your Namesulemanof <suleimandaniil@gmail.com>
2017-09-07 14:27:17 +0300

    final solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.min = 0;
+      this.max = 0;
+      this.k = 0;
+    }
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+       this.k = Math.ceil((this.min + this.max)/2);
+       return this.k;
     }
 
     lower() {
-
+      this.max = this.k;
     }
 
     greater() {
-
+      this.min = this.k;
     }
 }
 
